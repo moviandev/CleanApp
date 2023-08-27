90b88c0b2613e292ce5157c1af7be20463d7c161
Matheus Viana <matheus_o_viana@hotmail.com>
2023-08-27 16:57:39 -0300

    improve test name

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -14,7 +14,7 @@ class AlamofireAdapter {
 }
 
 final class AlamofireAdapterTests: XCTestCase {
-    func test_() {
+    func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
         let config = URLSessionConfiguration.default
         config.protocolClasses = [UrlProtocolStub.self]
